ad9feb5f0f55c39b8eee2b53fafd228bb66ed616
DanielAtonge <elamboatonge@gmail.com>
2022-10-01 19:04:31 +0300

    Week 05 exercises

--- week5/fifo_1.c
+++ week5/fifo_1.c
@@ -23,11 +23,13 @@ int main() {
         // We have opened the fifo 
         // for reading from another process
         // so we can write to it
-
-        if((write(fd, arr, strlen(arr) + 1)) == -1) {
-            return 2;
+        for (int i = 0; i < 2; i++){
+            if((write(fd, arr, strlen(arr) + 1)) == -1) {
+                return 2;
+            }
+            printf("Wrote %s\n", arr);
         }
-        printf("Wrote %s\n", arr);
+        
         close(fd);
     }
     return 0;
